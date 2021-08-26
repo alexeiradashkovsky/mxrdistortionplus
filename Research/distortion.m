0a8dbd8c5387a811983807d9fb448c7f93979983
alexei_prog_snob <gitlabalexei@gmail.com>
2021-08-26 18:26:33 +0300

    add realistic values for Vi

--- Research/distortion.m
+++ Research/distortion.m
@@ -1,41 +1,41 @@
 %%
-clc;clear; close all;
+
 %%
+clc;clear; close all;
 Fs = 44100;
 Ts = 1/Fs;
-f = 3;
-t = [0:Ts:1].';
+f = 1000;
+t = [0:Ts:0.01].';
 
-Vi = 1 * sin(2*pi*f*t);
+Vi = 0.1 * sin(2*pi*f*t);
 
 N = length(Vi);
 C1 = 47e-9;
 R1 = Ts/(2*C1);
 R3 = 4700;
 R4 = 1e6;
-Rp = 1e6; % max
+Rp = 0; % max
 
 G = 1 / (R1+R3+Rp);
 Gb = (R3+Rp) * G;
 Gi = 1 + R4*G;
 Gx1 = R1 * R4 * G;
-x1 = zeros(N,1);
-x2 = zeros(N,1);
+x1 = 0;
+x2 = 0;
 Vo = zeros(N,1);
-Vb = zeros(N,1);
-Vr1 = zeros(N,1);
-Vr2 = zeros(N,1);
+Vb = 0;
+Vr1 = 0;
 for n = 1:N
-   Vb(n,1) = Gb*Vi(n,1) - R1*Gb*x1(n,1);
-   Vr1(n,1) = Vi(n,1) - Vb(n,1);
-   Vo(n,1) = Gi*Vi(n,1) - Gx1*x1(n,1);
+   Vb = Gb*Vi(n,1) - R1*Gb*x1;
+   Vr1 = Vi(n,1) - Vb;
+   Vo(n,1) = Gi*Vi(n,1) - Gx1*x1;
    if Vo(n,1) > 4.5;
        Vo(n,1) = 4.5;
    elseif Vo(n,1) < -4.5;
        Vo(n,1) = -4.5;  
    end
    if n < N
-       x1(n+1,1) = (2 * Vr1(n,1) / R1) - x1(n,1);
+       x1 = (2 * Vr1 / R1) - x1;
    end
 end
 
